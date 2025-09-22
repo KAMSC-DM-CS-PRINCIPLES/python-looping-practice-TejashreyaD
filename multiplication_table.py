ccd056f9f5c870558ec9a8e6a733c5297f738efd
TejashreyaD <tejashreyad@gmail.com>
2025-09-22 19:07:31 -0400

    yes

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,5 +1,5 @@
 def multiplication_table(n):
     for i in range (1,n+1):
         for r in range (1, n+1):
-            print (i, "x", r, i*r)
+            print (i, "x", r, "=", i*r)
             print()
