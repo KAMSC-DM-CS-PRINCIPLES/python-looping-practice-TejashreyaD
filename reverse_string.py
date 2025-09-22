bf40d2c808a050e6ce6c0cda4a2e6e0d8845a70f
TejashreyaD <tejashreyad@gmail.com>
2025-09-22 19:47:08 -0400

    yes

--- reverse_string.py
+++ reverse_string.py
@@ -2,6 +2,6 @@ def reverse_string(text):
     reversed_string = ""
     index = len(text)-1
     while index>=0:
-        reversed_string+=s[index]
+        reversed_string+=text[index]
         index-=1
     return reversed_string
